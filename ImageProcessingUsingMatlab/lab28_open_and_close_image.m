56a6124349100029319d7fadb9a22519f364aae3
Hozan Ageed <57224535+hozan66@users.noreply.github.com>
2022-03-04 20:59:28 +0300

    fixing

--- ImageProcessingUsingMatlab/lab28_open_and_close_image.m
+++ ImageProcessingUsingMatlab/lab28_open_and_close_image.m
@@ -1,10 +1,12 @@
 clear
 close
 clc
+% Open= [A(-)B](+)B
+% Close [A(+)B](-)B
 
 % Morphology (working on white and black image)
 % original_image=imread('circbw.tif');
-original_image=imread('text.png');
+original_image=imread('coins.png');
 [row, col] = size(original_image);
 % Convert it to black and white image
 % original_image=gray2bw(original_image);
@@ -44,28 +46,57 @@ for i = r : row_dilation - (r-1)
         erosion_image_zero_border(i,j)=min(result);  %%Erostion
     end
 end
+%=========================================================================
+close_image_zero_border=dilation_image_zero_border;
+open_image_zero_border=erosion_image_zero_border;
+for i = r : row_dilation - (r-1)
+    for j = c : col_dilation - (c-1)
+        result=[];
+        result2=[];
+        index=1;
+        index2=1;
+        for p = 1 : row_SE
+            for q = 1 : col_SE
+                if(structure_element(p, q)==0)
+                    continue;
+                else
+                    result(index) =  structure_element(p, q) * erosion_image_zero_border(p + i-r, q + j-c);
+                    index=index+1;
+                    
+                    result2(index2) =  structure_element(p, q) * dilation_image_zero_border(p + i-r, q + j-c);
+                    index2=index2+1;
+                end
+            end
+        end
+        close_image_zero_border(i,j)=min(result2); %%Close Image
+        open_image_zero_border(i,j)=max(result);  %%Open Image
+    end
+end
+%========================================================================
 
 % Removing Zeros Padding
-dilation_image=dilation_image_zero_border(r:end-(r-1), c:end-(c-1));
-erosion_image=erosion_image_zero_border(r:end-(r-1), c:end-(c-1));
+close_image=close_image_zero_border(r:end-(r-1), c:end-(c-1));
+close_image=uint8(close_image);
+open_image=open_image_zero_border(r:end-(r-1), c:end-(c-1));
+open_image=uint8(open_image);
 
 % Show Images
 subplot(1,3,1);
 imshow(original_image); title('Origin Image');
 subplot(1,3,2);
-imshow(dilation_image); title('Dilation Image');
+imshow(open_image); title('Open Image');
 subplot(1,3,3);
-imshow(erosion_image); title('Erosion Image');
+imshow(close_image); title('Close Image');
 
 % =================================================================
 % Using built-in functions
-DI=imdilate(original_image,structure_element);
-EI=imerode(original_image,structure_element);
+OI=imopen(original_image,structure_element);
+CI=imclose(original_image,structure_element);
 figure,
 % Show built-in functions Images
 subplot(1,3,1);
 imshow(original_image); title('Origin Image');
 subplot(1,3,2);
-imshow(DI); title('built-in Dilation Image');
+imshow(OI); title('built-in Open Image');
 subplot(1,3,3);
-imshow(EI); title('built-in Erosion Image');
+imshow(CI); title('built-in Close Image');
